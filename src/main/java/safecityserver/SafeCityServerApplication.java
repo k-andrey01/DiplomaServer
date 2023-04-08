e97eee316a62224c692803cb2e58b877b2c418db
k-andrey01 <russia_andreykazantsev2001@mail.ru>
2023-04-09 01:04:10 +0300

    empty project

---
+++ src/main/java/safecityserver/SafeCityServerApplication.java
@@ -0,0 +1,13 @@
+package safecityserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SafeCityServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SafeCityServerApplication.class, args);
+    }
+
+}
