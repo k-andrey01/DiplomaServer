d19978063b42482d0ee7d9348e3bf86f1958e995
k-andrey01 <russia_andreykazantsev2001@mail.ru>
2023-05-19 23:40:26 +0300

    Добавлено хеширование паролей

---
+++ src/main/java/safecityserver/SecurityConfig.java
@@ -0,0 +1,16 @@
+package safecityserver;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+
+import javax.servlet.http.HttpServletRequest;
+
+@Configuration
+public class SecurityConfig {
+
+    @Bean
+    public BCryptPasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+}
--- src/main/java/safecityserver/controllers/UserController.java
+++ src/main/java/safecityserver/controllers/UserController.java
@@ -1,6 +1,8 @@
 package safecityserver.controllers;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.format.annotation.DateTimeFormat;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
 import safecityserver.entities.Userr;
@@ -14,15 +16,19 @@ import java.util.Optional;
 public class UserController {
     @Autowired
     private UserRepo userRepo;
+    @Autowired
+    private BCryptPasswordEncoder passwordEncoder;
 
     @PostMapping(path="/add")
     public @ResponseBody
     String addNewUser (@RequestParam String login, @RequestParam String password, @RequestParam String name,
-                       @RequestParam String surname, @RequestParam Date birthdate,
+                       @RequestParam String surname, @RequestParam @DateTimeFormat(pattern = "yyyy-MM-dd") Date birthdate,
                        @RequestParam String gender) {
         Userr user = new Userr();
         user.setLogin(login);
-        user.setPassword(password);
+        String hashedPassword = passwordEncoder.encode(password);
+        user.setPassword(hashedPassword);
+        //user.setPassword(password);
         user.setName(name);
         user.setSurname(surname);
         user.setBirthdate(birthdate);
